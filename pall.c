010e7c365e571383205ce261142941400ae9a4e3
juanjts <1843@holbertonschool.com>
2020-09-16 19:20:36 +0000

    task

---
+++ pall.c
@@ -0,0 +1,18 @@
+#include "header.h"
+#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+extern struct var_global global_t;
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = *stack;
+	(void) line_number;
+    
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
--- push.c
+++ push.c
@@ -7,8 +7,29 @@ extern struct var_global global_t;
 
 void push(stack_t **stack, unsigned int line_number)
 {
+    stack_t *new;
+	stack_t *tmp;
     (void)stack;
     (void)line_number;
 
-    printf("ESO HiJUEPUTA\n");
+    if (!global_t.strings[1])
+        printf("error");
+    new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		printf("error");
+    new->n = atoi(global_t.strings[1]);
+    new->next = NULL;
+    if (*stack == NULL)
+	{
+		new->prev = NULL;
+		*stack = new;
+	}
+    else
+	{
+		tmp = *stack;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+		new->prev = tmp;
+	}
 }
