6029bf64ecd53b53c99c631cde62493fe0ce6738
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-09-17 14:14:47 -0500

    hae el pull

---
+++ e_handler.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct var_global global_t;
+
+void e_handler(int error)
+{
+	if (error == 1)
+	{
+		
+	}
+	else if (error == 2)
+	{
+
+	}
+	else if (error == 3)
+	{
+
+	}
+	else if (error = 4)
+	{
+
+	}
+	else if (error = 5)
+	{
+
+	}
+}
--- main.c
+++ main.c
@@ -3,42 +3,41 @@
 #include <stdlib.h>
 #include <string.h>
 struct var_global global_t;
-
-void main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-    size_t line_size = 0;
-    int read = 0, arg = 0;
-    FILE *fd;
-    char *token, *line = NULL;
-    void (*operation)(stack_t **, unsigned int);
-    global_t.nodo = NULL, global_t.strings = NULL;
+	size_t line_size = 0;
+	int read = 0, arg = 0;
+	FILE *fd;
+	char *token, *line = NULL;
+	void (*operation)(stack_t **, unsigned int);
 
+	global_t.nodo = NULL, global_t.strings = NULL;
 
-    if (argc == 2)
-    {
-        fd = fopen(argv[1], "r");
-
-        if (!fd)
-            printf("couln't open the file");
-    }
-    else
-        return (1);
-    global_t.strings = malloc(sizeof(char *) * 100);
-    if (!global_t.strings)
-        return (-1);
-    read = getline(&line, &line_size, fd);
-    while (read > 0)
-    {
-        token = strtok(line, " \n\t\r\a");
-        for (; token != NULL; token = strtok(NULL, " \n\t\r\a"))
-        {
-            global_t.strings[arg] = strdup(token);
-            arg++;
-        }
-        arg = 0;
-        global_t.n_linea++;
-        operation = unificador(global_t.strings, global_t.n_linea);
-        operation(&global_t.nodo, global_t.n_linea);
-        read = getline(&line, &line_size, fd);
-    }
+	if (argc == 2)
+	{
+		fd = fopen(argv[1], "r");
+		if (!fd)
+			printf("couln't open the file");
+	}
+	else // PRINT USAGE ERROR
+		exit(EXIT_FAILURE);
+	global_t.strings = malloc(sizeof(char *) * 100);
+	if (!global_t.strings)
+		exit(EXIT_FAILURE);
+	read = getline(&line, &line_size, fd);
+	while (read > 0)
+	{
+		token = strtok(line, " \n\t\r\a");
+		for (; token != NULL; token = strtok(NULL, " \n\t\r\a"))
+		{
+			global_t.strings[arg] = strdup(token);
+			arg++;
+		}
+		arg = 0;
+		global_t.n_linea++;
+		operation = unificador(global_t.strings, global_t.n_linea);
+		operation(&global_t.nodo, global_t.n_linea);
+		read = getline(&line, &line_size, fd);
+	}
+	return (1);
 }
